451a68e9e9f62f5cf5aba8187add49b00bf5ebe5
JesusFrelseren <HailTheUser@gmail.com>
2015-02-27 13:22:53 +0100

    Added protoype

---
+++ assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	public float paddleSpeed = 1f;
+	private Vector3 playerPos = new Vector3(0, -9.5f, 0);
+	
+	void Update () {
+		float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -7f, 7f), -9.5f, 0f);
+		transform.position = playerPos;
+	}
+
+}
