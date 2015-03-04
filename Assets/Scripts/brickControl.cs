072af7a70d5b065bebc0701881901af206440764
TommyBal <ballestadtommy@gmail.com>
2015-03-04 21:21:04 +0100

    BrickController

---
+++ Assets/Scripts/brickControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class brickControl : MonoBehaviour {
+
+    public int score = 10;
+    public int health = 1;
+
+    private Text scoreText;
+
+    void Awake()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        health--;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
