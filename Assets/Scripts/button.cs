7c3840042c5e9555c77495ceb34008919f19705c
thanglyphan <lyern52@gmail.com>
2015-03-04 21:13:42 +0100

    button

---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+	void OnGUI(){
+		if (GUI.Button (new Rect (10, 10, 100, 30), "Pause")) {
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+			}
+			else
+			{
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
