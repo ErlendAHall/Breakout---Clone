2874bf1835a768f983116b419503233a1fffffdd
TommyBal <ballestadtommy@gmail.com>
2015-03-04 16:12:09 +0100

    Paddle controller

---
+++ Assets/Scripts/paddleControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paddleControl : MonoBehaviour
+{
+
+    public float paddleSpeed = 0.5f;
+    private Vector2 paddlePosition = new Vector2(0f, -4.25f);
+    private float clampLenght = 8f;
+
+    void Start()
+    {
+        transform.position = new Vector2(0, -4.25f);
+    }
+
+    void Update()
+    {
+        float newPosition = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        paddlePosition = new Vector2(Mathf.Clamp(newPosition, clampLenght - (clampLenght * 2), clampLenght), -4.25f);
+        transform.position = paddlePosition;
+    }
+}
