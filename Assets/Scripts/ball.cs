1e6b3925d3b0ba36a989a36601d4efa7de3e7e39
TommyBal <ballestadtommy@gmail.com>
2015-03-04 17:37:54 +0100

    Lagt til ball, og litt spretting

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour
+{
+
+    public float ballInitSpeed = 600f;
+
+    private Rigidbody2D playBall;
+    private bool inPlay = false;
+    public float currentSpeed = 0;
+    void Awake()
+    {
+        playBall = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            transform.parent = null;
+            inPlay = true;
+            playBall.AddForce(new Vector2(ballInitSpeed, -ballInitSpeed));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        currentSpeed = Vector3.Magnitude(rigidbody2D.velocity);
+        if (currentSpeed > 10f)
+        {
+            rigidbody2D.velocity /= currentSpeed / 10f;
+        }
+        if (currentSpeed < 5f && currentSpeed != 0)
+        {
+            rigidbody2D.velocity /= currentSpeed / 5f;
+        }
+    }
+}
