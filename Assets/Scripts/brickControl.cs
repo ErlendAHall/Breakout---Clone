e5b694879d97098b04b4a1d5c6a3ddb7ae3a2eba
TommyBal <ballestadtommy@gmail.com>
2015-03-04 21:42:41 +0100

    Litt endringer i bricks

--- Assets/Scripts/brickControl.cs
+++ Assets/Scripts/brickControl.cs
@@ -7,19 +7,23 @@ public class brickControl : MonoBehaviour {
     public int score = 10;
     public int health = 1;
 
-    private Text scoreText;
-
-    void Awake()
-    {
-
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Scored " + score + " points");
         health--;
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    void setScore(int score)
+    {
+        this.score = score;
+    }
+
+    void setHealth(int health)
+    {
+        this.health = health;
     }
 }
--- Assets/Scripts/brickSpawn.cs
+++ Assets/Scripts/brickSpawn.cs
@@ -18,13 +18,17 @@ public class brickSpawn : MonoBehaviour {
         Transform _t = GetComponent<Transform>();
         float currentY = _t.position.y;
         float currentX = _t.position.x;
+        int count = 1;
         for (float y = currentY; y < currentY+bricksInY; y++)
         {
             for (float x = currentX; x < currentX+bricksInX; x++)
             {
                 GameObject brick = (Instantiate(prefabBrick, new Vector3(x*0.7f, y*0.5f, 0), Quaternion.identity)) as GameObject;
                 brick.GetComponent<SpriteRenderer>().sprite = brickSprites[(int)y];
+                brick.GetComponent<brickControl>().score = count*10;
+                brick.GetComponent<brickControl>().health = count;
             }
+            count++;
         }
 	}
 }
