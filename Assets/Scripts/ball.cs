cb01a8292a6b78d1899c9b167aa18af159f3dcdb
TommyBal <ballestadtommy@gmail.com>
2015-03-05 07:05:16 +0100

    Samme som siste... Endringene alt kom ikke med

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -5,10 +5,12 @@ public class ball : MonoBehaviour
 {
 
     public float ballInitSpeed = 600f;
+    public float ballMaxSpeed = 15f;
+    public float ballMinSpeed = 7f;
 
     private Rigidbody2D playBall;
     private bool inPlay = false;
-    public float currentSpeed = 0;
+    private float currentSpeed = 0;
     void Awake()
     {
         playBall = GetComponent<Rigidbody2D>();
@@ -27,18 +29,22 @@ public class ball : MonoBehaviour
     void FixedUpdate()
     {
         currentSpeed = Vector3.Magnitude(rigidbody2D.velocity);
-        if (currentSpeed > 10f)
+        if (currentSpeed > ballMaxSpeed)
         {
-            rigidbody2D.velocity /= currentSpeed / 10f;
+            rigidbody2D.velocity /= currentSpeed / ballMaxSpeed;
         }
-        if (currentSpeed < 5f && currentSpeed != 0)
+        if (currentSpeed < ballMinSpeed && currentSpeed != 0)
         {
-            rigidbody2D.velocity /= currentSpeed / 5f;
+            rigidbody2D.velocity /= currentSpeed / ballMinSpeed;
         }
         if (playBall.position.y < -4.25f)
         {
             GameObject.FindGameObjectWithTag("gm").GetComponent<gameController>().decreaseLife();
-            playBall.position = new Vector2(0, -1);
+            transform.parent = GameObject.FindGameObjectWithTag("paddle").GetComponent<Transform>();
+            transform.localPosition = new Vector2(0, 0.5f);
+            playBall.velocity = Vector2.zero;
+            playBall.angularVelocity = 0f;
+            inPlay = false;
         }
     }
 }
--- Assets/Scripts/paddleControl.cs
+++ Assets/Scripts/paddleControl.cs
@@ -6,7 +6,7 @@ public class paddleControl : MonoBehaviour
 
     public float paddleSpeed = 0.5f;
     private Vector2 paddlePosition = new Vector2(0f, -4.25f);
-    private float clampLenght = 7.3f;
+    private float clampLenght = 6.8f;
 
     void Start()
     {
@@ -18,5 +18,11 @@ public class paddleControl : MonoBehaviour
         float newPosition = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
         paddlePosition = new Vector2(Mathf.Clamp(newPosition, clampLenght - (clampLenght * 2), clampLenght), -4.25f);
         transform.position = paddlePosition;
+    }
+
+    public void decreaseScale()
+    {
+        transform.localScale = new Vector3(transform.localScale.x / 2, transform.localScale.y, transform.localScale.z);
+        clampLenght += 0.6f;
     }
 }
