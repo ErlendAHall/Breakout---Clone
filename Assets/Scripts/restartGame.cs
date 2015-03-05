56b00e4289111c0ddb94adcd9e40deab444c669a
TommyBal <ballestadtommy@gmail.com>
2015-03-05 07:27:45 +0100

    Lagt til muligheten til å restarte ved game over

---
+++ Assets/Scripts/restartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class restartGame : MonoBehaviour {
+
+    public void restart()
+    {
+        Application.LoadLevel("mainScene");
+    }
+}
