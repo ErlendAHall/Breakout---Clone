12e78b54cab0cea4236e9d1d9f42d8548f329177
thanglyphan <lyern52@gmail.com>
2015-03-08 20:41:20 +0100

    DONE

--- Assets/Scripts/pauseButton.cs
+++ Assets/Scripts/pauseButton.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class pauseButton : MonoBehaviour {
 	void OnGUI(){
-		if(GUI.Button(new Rect(10,10,100,30), "Pause")){
+		if(GUI.Button (new Rect (Screen.width/2 - 50, Screen.height/2 - 320, 50, 30), "Pause")){
 			if(Time.timeScale == 1)
 			{
 				Time.timeScale = 0;
